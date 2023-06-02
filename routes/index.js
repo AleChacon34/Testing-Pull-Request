8ba1925ab805a9536728c5a2a9f7db97e1adfe95
AleChacon <achacon81.ac@gmail.com>
2023-06-02 14:50:01 -0600

    Tests

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
-const PATH_ROUTER = __dirname;
+const PATH_ROUTES = __dirname;
 
-const removeExtension = (filename) => {
-    return filename.split('.').shift();
+const removeExtension = (fileName) => {
+    return fileName.split('.').shift();
 }
 
-fs.readdirSync(PATH_ROUTER).filter((file) => {
+fs.readdirSync(PATH_ROUTES).filter((file) => {
     const name = removeExtension(file);
     if (name != 'index') {
-        router.use(`/${name}`, require(`.${file}`));
+        router.use(`/${name}`, require(`./${file}`));
     }
 });
 
